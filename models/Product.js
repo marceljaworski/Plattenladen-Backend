448323425c9ad9e9a207acab2e02b6eb84bdf5ce
Marcelearn <jaworskimarcel@gmail.com>
2023-02-07 14:30:31 +0100

    commentars deleted

--- models/Product.js
+++ models/Product.js
@@ -25,30 +25,5 @@ export const getAll = async () => {
     const products = await Product.find();
     return products;
 };
-// export const create = async (document) => {    
-//     const newUser = new User(document);
-//     const result = await newUser.save();
-//     return result;
-    
-// };
-// export const getOne = async (filter) => {
-//     const user = await User.findOne(filter);
-//     return user;
-// };
-// export const replace = async (userId, data) => {
-//     const user = await User.findOneAndReplace({_id: userId}, data, {returnDocument: "after", runValidators: true},);
-
-//     return user;
-// };
-// export const update = async (userId, data) => {
-//     const user = await User.findByIdAndUpdate(userId, data, {new: true, runValidators: true});
-
-//     return user;
-// };
-// export const deleteOne = async (userId) => {
-//     const user = await User.findByIdAndDelete(userId)
-
-//     return user;
-// };
 
 export default User;
