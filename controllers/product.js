9d11353ee6fa7b052eb84890684ae7bae4312044
Marcelearn <jaworskimarcel@gmail.com>
2023-02-08 15:35:00 +0100

    Faker.js added

--- controllers/product.js
+++ controllers/product.js
@@ -1,10 +1,8 @@
-import * as User from "../models/Product.js";
-
-
+import * as Product from "../models/Product.js";
 
 export const getAll = async (req, res, next) => {
     try {
-        const result = await User.getAll();
+        const result = await Product.getAll();
         res.status(200).json(result);
     } catch (error) {
         next(error);
