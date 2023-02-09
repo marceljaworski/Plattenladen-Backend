5ee866b4283ac751d4da334bae4ff0c556381187
Marcelearn <jaworskimarcel@gmail.com>
2023-02-09 14:25:16 +0100

    user model added

---
+++ models/User.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        validate: {
+            validator: (v) => /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(v),
+            message: "Please enter a valid email address"
+        }
+    },
+    password: String,
+    
+},{versionKey: false})
+const User = mongoose.model("User", userSchema);
+
+// export const getAll = async () => {
+//     const users = await User.find();
+//     return users;
+// };
+export const create = async (document) => {    
+    const newUser = new User(document);
+    const result = await newUser.save();
+    return result;
+    
+};
+export const getOne = async (filter) => {
+    const user = await User.findOne(filter);
+    return user;
+};
+// export const replace = async (userId, data) => {
+//     const user = await User.findOneAndReplace({_id: userId}, data, {returnDocument: "after", runValidators: true},);
+
+//     return user;
+// };
+// export const update = async (userId, data) => {
+//     const user = await User.findByIdAndUpdate(userId, data, {new: true, runValidators: true});
+
+//     return user;
+// };
+// export const deleteOne = async (userId) => {
+//     const user = await User.findByIdAndDelete(userId)
+
+//     return user;
+// };
+
+export default User;
