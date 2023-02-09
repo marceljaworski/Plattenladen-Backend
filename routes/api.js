ecb42572d40926f33ccc5fe51d2bed47a33377e1
Marcelearn <jaworskimarcel@gmail.com>
2023-02-09 14:37:56 +0100

    login and register endpoints

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import * as product from "../controllers/product.js";
+import * as user from "../controllers/user.js"
 
 const apiRoutes = Router();
 
 apiRoutes.get("/products", product.getAll);
+apiRoutes.post("/register", user.create);
+apiRoutes.post("/login", user.login);
 
 
 export default apiRoutes;
