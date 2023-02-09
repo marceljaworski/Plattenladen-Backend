677d312a18fb6e497cbd47b7a1292d19dd57e723
Marcelearn <jaworskimarcel@gmail.com>
2023-02-09 14:49:56 +0100

    validate user's register

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import * as product from "../controllers/product.js";
-import * as user from "../controllers/user.js"
+import * as user from "../controllers/user.js";
+import validate from "../middlewares/validate.js";
+import { postUser } from "./user.schema.js";
 
 const apiRoutes = Router();
 
 apiRoutes.get("/products", product.getAll);
-apiRoutes.post("/register", user.create);
-apiRoutes.post("/login", user.login);
+apiRoutes.post("auth/register", validate(postUser), user.create);
+apiRoutes.post("auth/login", user.login);
 
 
 export default apiRoutes;
