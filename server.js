cf6c88c668c454002f55dc162e94a570e2539f60
Marcelearn <jaworskimarcel@gmail.com>
2023-02-13 15:59:12 +0100

    cors in

--- server.js
+++ server.js
@@ -3,17 +3,20 @@ dotenv.config();
 import express from "express";
 import logMiddleware from "./middlewares/log.js";
 import apiRoutes from "./routes/api.js"
+import cors from "cors";
 
 import "./lib/mongoose.js";
 
 const app = express();
 app.use(express.json());
 app.use(logMiddleware);
+app.use(cors())
 
 const port = process.env.PORT || 4003;
 app.listen(port, () => console.log(`listening on port ${port}`));
 
 app.use("/api", apiRoutes);
+app.use(express.static("dist"));
 
 app.use((error, req, res, next) => {
     console.error(error);
