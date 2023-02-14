249b02350e00bd7c6f76846166c651c86d88cb0b
Marcelearn <jaworskimarcel@gmail.com>
2023-02-14 17:25:13 +0100

    path for backend(dist) fixed

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ import apiRoutes from "./routes/api.js"
 import cors from "cors";
 
 import "./lib/mongoose.js";
-
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 app.use(express.json());
 app.use(logMiddleware);
@@ -16,7 +19,10 @@ const port = process.env.PORT || 4003;
 app.listen(port, () => console.log(`listening on port ${port}`));
 
 app.use("/api", apiRoutes);
-app.use(express.static("dist"));
+app.use("/", express.static("./dist"));
+
+app.get("/*", (req, res) => res.sendFile(__dirname + "/dist/index.html"));
+
 
 app.use((error, req, res, next) => {
     console.error(error);
